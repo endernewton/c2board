b02d2742473ce5e1a371fff5ebb13e1630ecd04c
Xinlei Chen <xinleic@devfair015.maas>
2018-03-06 17:05:53 -0800

    initial commit.

---
+++ Makefile
@@ -0,0 +1,14 @@
+.PHONY: default
+default: all
+
+.PHONY: install
+install:
+	python2 setup.py install
+
+.PHONY: all
+all:
+	protoc c2board/src/*.proto --python_out=.
+
+.PHONY: clean
+clean:
+	rm -rf c2board/src/*_pb2.py
---
+++ setup.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+
+with open('HISTORY.rst') as history_file:
+    history = history_file.read()
+
+requirements = [
+    'numpy',
+    'protobuf >= 0.3.2',
+    'six',
+]
+
+test_requirements = [
+    'pytest',
+]
+
+setup(
+    name='c2board',
+    version='1.1',
+    description='TensorBoardX lets you watch Tensors Flow without Tensorflow',
+    long_description= history,
+    author='Tzu-Wei Huang',
+    author_email='huang.dexter@gmail.com',
+    url='https://github.com/lanpa/tensorboard-pytorch',
+    packages=find_packages(exclude=['docs', 'tests']),
+    include_package_data=True,
+    install_requires=requirements,
+    license='MIT license',
+    zip_safe=False,
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',        
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+    test_suite='tests',
+    tests_require=test_requirements
+)
+
+# python setup.py sdist bdist_wheel --universal
+# twine upload dist/*
